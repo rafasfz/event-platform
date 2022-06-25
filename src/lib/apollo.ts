54e78bcc40b5bd6a484d7a631fc0e7289c3c2ea1
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-25 18:36:31 -0300

    Creates subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oyjmfy1g9k01xk9byjhb2h/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
